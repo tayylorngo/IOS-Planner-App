2b7dcc4e52a57e442459a14790f6742b6f8b620b
Taylor Ngo <cs.taylorngo@gmail.com>
2021-06-22 00:20:28 -0400

    added calendar

--- Planner App/ViewController.swift
+++ Planner App/ViewController.swift
@@ -6,13 +6,51 @@
 //
 
 import UIKit
+import FSCalendar
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, FSCalendarDelegate, FSCalendarDataSource {
+    
+    @IBOutlet var calendar: FSCalendar!
+    var items: [ToDoListItem] = []
+    var dates: [Date] = []
+    var dateStrings: [String] = []
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    let dateFormatter = DateFormatter()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        calendar.delegate = self
+        calendar.dataSource = self
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        getAllItems()
+        for item in items {
+            dates.append(item.date ?? Date())
+        }
+        for date in dates {
+            dateStrings.append(self.dateFormatter.string(from: date))
+        }
     }
+    
+    func calendar(_ calendar: FSCalendar, numberOfEventsFor date: Date) -> Int {
+        let dateString = self.dateFormatter.string(from: date)
+        if self.dateStrings.contains(dateString) {
+            return 1
+        }
+        return 0
+    }
+    
+    func getAllItems() {
+        do{
+            items = try context.fetch(ToDoListItem.fetchRequest())
+        }
+        catch{
+            print("Error")
+        }
+    }
+    
+    
+    
+    
     
 
 
